ff12f47ba500e19479852471705aca7885e368ee
jonywu <jonywu@mitac.com.tw>
2023-12-21 21:25:45 +0800

    微卡關

--- src/views/MatchGame/MatchGame.vue
+++ src/views/MatchGame/MatchGame.vue
@@ -10,9 +10,9 @@
 	//  5. 將卡片獨立抽出為 Card.vue 元件
 
 	const cards = ref([]);          //紀錄開局卡片
-	const openedCard = ref([]);		//紀錄打開的卡片
-	const compareCards = ref([]);	//卡片比較區
+	const openedCards = ref([]);	//紀錄打開的卡片
 	const clearedCards = ref([]);	//紀錄對對碰成功的卡片
+
 
 	// 遊戲初始化，洗牌
 	const gameInit = () => {
@@ -21,40 +21,13 @@
 		numArr.sort(() => Math.random() - 0.5);
 		console.log(numArr);
 		cards.value = numArr.map(d => (d % 8) + 1);
-		openedCard.value = [];
+
+		openedCards.value = [];
 		clearedCards.value = [];
 	}
 	
-	const clickHandler = (idx, n) => {   
 
-		openedCard.value.push(idx)
-		compareCards.value.push(n)
-
-		if (compareCards.value[1] != null) {
-			if (compareCards.value[0] == compareCards.value[1]) {
-				//比對正確 goes here
-				compareCards.value = [];
-				clearedCards.value = [...openedCard.value];
-
-				if (clearedCards.value.length === cards.value.length) {
-					
-					window.setTimeout(() => {
-						const result = window.confirm('好樣的！恭喜破關，再來一局？');
-						if (result) {
-							gameInit();
-						}
-					}, 1000);
-				}
-			} else {
-				//比對失敗 goes here
-				window.setTimeout(() => {
-					openedCard.value.splice(-2);
-				}, 1000);
-				compareCards.value = [];
-			}
-		}
-		
-	}
+	
 </script>
 
 <template>
@@ -68,26 +41,16 @@
 		</div>
 
 		<div class="rounded-xl mx-auto border-4 mt-12 grid grid-flow-col p-10 w-[900px] gap-2 grid-rows-4">
-		
-		<div 
-			v-for="(n, idx) in cards"
-			class="flip-card transition ease-in-out hover:-translate-y-1 hover:scale-110"
-			:class="{
-				'open': openedCard.includes(idx),
-				'transition ease-in-out opacity-0 delay-1000': clearedCards.includes(idx)
-			}"
-			@click="clickHandler(idx, n)">
-			idx:{{ idx }}  n:{{ n }}
-			<div class="flip-card-inner" v-if="cards[idx] > 0">
-			<div class="flip-card-front"></div>
-			<div class="flip-card-back">
-				<img :src="`./img/cat-0${n}.jpg`" alt="">
-			</div>
-			</div>
-		</div>
+			<!-- 插入元件 -->
+			<Card 
+				:cards="cards"
+				:openedCards="openedCards"
+				:clearedCards="clearedCards"
+				
+				/>
 
 		</div>
 	</div>
 </template>
 
-<style scoped src="./MatchGame.css"></style>
+<!-- <style scoped src="./MatchGame.css"></style> -->
--- src/views/MatchGame/component/Card.vue
+++ src/views/MatchGame/component/Card.vue
@@ -1,15 +1,70 @@
-<template>
-
-
-
-</template>
-
 <script setup>
+    import { ref } from 'vue';
 
+    const props = defineProps({
+        cards: Array,
+        openedCards: Array,
+        clearedCards: Array,
+    });
+    
+    props.clearedCards.push(100)
+    // ============ 變數宣告 ============ //
+    const compareCards = ref([]);	//卡片比較區
+    console.log('props.cards.value', props.cards);
 
+    console.log('props.clearedCards', props.clearedCards);
+    // ============ 點擊事件 ============ //
+    const clickHandler = (idx, n) => {
+        
+
+        props.openedCards.push(idx)
+        compareCards.value.push(n)
+
+        if (compareCards.value[1] != null) {
+            if (compareCards.value[0] == compareCards.value[1]) {
+                //比對正確 goes here
+                compareCards.value = [];
+                props.clearedCards = [...props.openedCards];
+
+                if (props.clearedCards.length === props.cards.length) {
+                    console.log("finished~~~~~~~");
+                    window.setTimeout(() => {
+                        const result = window.confirm('好樣的！恭喜破關，再來一局？');
+                        if (result) {
+                            gameInit();
+                        }
+                    }, 1000);
+                }
+            } else {
+                //比對失敗 goes here
+                window.setTimeout(() => {
+                    props.openedCards.splice(-2);
+                }, 1000);
+                compareCards.value = [];
+            }
+            // props.clearedCards.push(...props.openedCards);
+        }
+    }
 
 </script>
 
-<style lang="scss" scope>
-  
-</style>
+
+<template>
+    <div v-for="(n, idx) in cards" 
+        class="flip-card transition ease-in-out hover:-translate-y-1 hover:scale-110" 
+        :class="{
+            'open': openedCards.includes(idx),
+            'transition ease-in-out opacity-0 delay-1000': clearedCards.includes(idx)
+        }" 
+        @click="clickHandler(idx, n)">
+        idx:{{ idx }} n:{{ n }}
+        <div class="flip-card-inner" v-if="cards[idx] > 0">
+            <div class="flip-card-front"></div>
+            <div class="flip-card-back">
+                <img :src="`./img/cat-0${n}.jpg`" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped src="../MatchGame.css"></style>
